da96d50f49daa52fc5159f22a90859e49ddf001c
Chris <c.lamothe25@gmail.com>
2019-01-24 22:04:46 -0500

    Trying

--- indecisionApp/public/scripts/app.js
+++ indecisionApp/public/scripts/app.js
@@ -60,7 +60,8 @@ var template = React.createElement(
 
 var count = 0;
 var addOne = function addOne() {
-    console.log('addOne');
+    count++;
+    console.log('addOne', count);
 };
 
 var minusOne = function minusOne() {
--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -46,7 +46,8 @@ const template = (
 
 let count = 0
 const addOne = () => {
-    console.log('addOne')
+    count ++
+    console.log('addOne', count)
 }
 
 const minusOne = () => {
